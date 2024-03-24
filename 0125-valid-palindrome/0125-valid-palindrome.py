89b47a8738dc39e1e0e46d601077ac7e649a1383
Neo Jarmawijaya <78681517+neojarma@users.noreply.github.com>
2024-03-24 17:00:22 +0700

    Time: 57 ms (11.26%), Space: 16.9 MB (91.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        
+        while l < r:
+            a = s[l].lower()
+            b = s[r].lower()
+            
+            if a.isalnum() and b.isalnum():
+                if a != b:
+                    return False
+            elif not a.isalnum():
+                l += 1
+                continue
+            elif not b.isalnum():
+                r -= 1
+                continue
+            
+            l += 1 
+            r -= 1
+        
+        return True
+            
+    def isAlphaNum(self, letter):
+        return letter.isalnum()
