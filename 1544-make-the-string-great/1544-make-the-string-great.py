26547b66706fd0e5b8f60ec6c92906a462385bee
Neo Jarmawijaya <78681517+neojarma@users.noreply.github.com>
2024-04-06 11:37:03 +0700

    Time: 42 ms (27.84%), Space: 16.6 MB (47.01%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,39 +1,17 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        s = list(s)
-        curr = 0
-        length = len(s)
+        stack = []
         
-        while curr < length:
-            if len(s) < 2:
-                break
-            if s[curr].isupper():
-                if curr == 0:
-                    if s[curr+1].upper() == s[curr] and s[curr+1] != s[curr]:
-                        s.pop(curr+1)
-                        s.pop(curr)
-                        curr = 0
-                        length -= 2
-                        continue
-                elif curr == length-1:
-                    if s[curr-1].upper() == s[curr] and s[curr] != s[curr-1]:
-                        s.pop()
-                        s.pop()
-                        length -= 2
-                        continue
-                else:
-                    if s[curr+1].upper() == s[curr] and s[curr+1] != s[curr]:
-                        s.pop(curr+1)
-                        s.pop(curr)
-                        curr = 0
-                        length -= 2
-                        continue  
-                    elif s[curr-1].upper() == s[curr] and s[curr] != s[curr-1]:
-                        s.pop(curr-1)
-                        s.pop(curr-1)
-                        curr = 0 if curr < 2 else curr - 2
-                        length -= 2
-                        continue
-            curr+=1
-        return ''.join(s)
+        for x in s:
+            if stack:
+                equalLower = stack[-1].lower() == x.lower()
+                equalUpper = stack[-1].upper() == x.upper()
+                badString = stack[-1] != x
+                
+                if equalLower and equalUpper and badString:
+                    stack.pop()
+                    continue
+            
+            stack.append(x)
         
+        return ''.join(stack)
