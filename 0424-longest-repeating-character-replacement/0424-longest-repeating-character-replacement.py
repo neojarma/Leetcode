8c74d9da3ba09c4e9446d75d288e0982b4210e15
Neo Jarmawijaya <78681517+neojarma@users.noreply.github.com>
2024-04-21 14:06:58 +0700

    Time: 80 ms (78.32%), Space: 16.6 MB (49.33%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,10 +3,11 @@ class Solution:
         res = 0
         freq = {}
         l = 0
-        
+        maxF =  0
         for r in range(len(s)):
             freq[s[r]] = freq.get(s[r], 0) + 1
-            isValid = (r - l + 1) - max(freq.values()) <= k
+            maxF = max(maxF, freq[s[r]])
+            isValid = (r - l + 1) - maxF <= k
             if isValid:
                 res = max(res, (r-l+1))
                 continue
